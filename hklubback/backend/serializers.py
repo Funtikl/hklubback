f92eeb3ec72c1830ceda025910e935ca605bfb29
Fuad Rustamzadeh <rustam.fuad@gmail.com>
2018-10-26 00:25:23 +0400

    serializers done

--- hklubback/backend/serializers.py
+++ hklubback/backend/serializers.py
@@ -1,14 +1,25 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from hklubback.backend.models import *
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'groups')
+        model = Users
+        fields = ('firstmname', 'lastname', 'email', 'groups_id')
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class InnerPeopleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Group
-        fields = ('url', 'name')
+        model = Inner_people
+        fields = ('name_az', 'name_ru','name_eng','story_az','story_ru','story_eng','photo','email','adress','money_stat')
+
+class UsersInneedPeopleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User_Inned_People
+        fields = ('user_id', 'Inneed_people_id', 'date')
+
+class PostAzSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post_az
+        fields = ('title', 'text', 'photo', 'Inneed_people_id', 'created_at', 'updated_at')
+        
